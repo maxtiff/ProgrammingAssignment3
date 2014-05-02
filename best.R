8e78c5cd2565fc7a3a00ef622b8902cfe3c32334
Travis <travismay@gmail.com>
2014-05-02 15:57:28 -0500

    Modify best.R

--- best.R
+++ best.R
@@ -1,4 +1,9 @@
 best <- function(state, outcome) {
+  ## alter case in user inputs for uniformity throughout program.
+  outcome <- tolower(outcome)
+  state <- toupper(state)
+  outcomeVect <- c("heart attack", "heart failure", "pneumonia")
+  
   ## Read outcome data and create subset by mortality outcome for heart attack, heart failure and pneumonia.
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
@@ -11,15 +16,24 @@ best <- function(state, outcome) {
   ## Create dataset of all hospitals
   allHospitals <- data.frame(hospital=vect1, state=vect2, heartAttack=vect3, heartFailure=vect4, pneumonia=vect5)
   
-  head(allHospitals)
+  ## Remove any rows with NA from the data frame.
+  completeHospitals <- allHospitals[complete.cases(allHospitals),]
   
-  ## Check that state and outcome are valid
-
+  ## Check that user inputs for state and outcome are valid.
+  if (!(state %in% completeHospitals$state)) {
+    
+    stop("Invalid state.")
+    
+  } else if (!(outcome %in% outcomeVect)) {
+    
+    stop("Invalid outcome.")
+    
+  }
   
-
   ## Return hospital name in that state with lowest 30-day death
   ## rate. Any ties are handled via alphabetization.
-  
-  
+   stateHospitals <- completeHospitals[completeHospitals$state == state,]
+   head(stateHospitals)
+
 }
 
