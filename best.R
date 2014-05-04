2e39bebe53ebb40a15807daebdf745d8081a7f6b
maxtiff <travismay@gmail.com>
2014-05-04 13:12:50 -0500

    Modify best.R

--- best.R
+++ best.R
@@ -3,7 +3,7 @@ best <- function(state, outcome) {
   outcome <- tolower(outcome)
   state <- toupper(state)
   
-  outcomeVect <- c("heart attack", "heart failure", "pneumonia")
+  outcomes <- c("heart attack", "heart failure", "pneumonia")
   
   ## Read outcome data and create subset by mortality outcome for heart attack, heart failure and pneumonia.
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
@@ -15,7 +15,7 @@ best <- function(state, outcome) {
   vect5 <- as.numeric(data[,23])
   
   ## Create dataset of all hospitals
-  allHospitals <- data.frame(hospital=vect1, state=vect2, heartAttack=vect3, heartFailure=vect4, pneumonia=vect5)
+  allHospitals <- data.frame(hospital=vect1, state=vect2, heart.attack=vect3, heart.failure=vect4, pneumonia=vect5)
   
   ## Remove any rows with NA from the data frame.
   completeHospitals <- allHospitals[complete.cases(allHospitals),]
@@ -25,7 +25,7 @@ best <- function(state, outcome) {
     
     stop("Invalid state.")
     
-  } else if (!(outcome %in% outcomeVect)) {
+  } else if (!(outcome %in% outcomes)) {
     
     stop("Invalid outcome.")
     
@@ -34,8 +34,9 @@ best <- function(state, outcome) {
   ## Return hospital name in that state with lowest 30-day death
   ## rate. Any ties are handled via alphabetization.
   stateHospitals <- completeHospitals[completeHospitals$state == state,]
-
-  
+  stateHospitals <- subset(stateHospitals, select = c(hospital,state,pneumonia))
+ 
+  head(stateHospitals)
   
 }
 
